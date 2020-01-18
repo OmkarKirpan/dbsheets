c06c623338ca9dcf5f910c86e885c8939cd08035
Omkar Kirpan <okirpan@gmail.com>
2020-01-19 00:46:19 +0530

    TypeAdapter registered

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:dbsheets/myhome.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:dbsheets/model/user.dart';
 
 void main() async {
   final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDir.path);
+  Hive.registerAdapter(UserAdapter());
   runApp(MyApp());
 }
 
